52ebb2385e09dcbe5c064aab1a6a82b2aa404644
Brent Abrahams <brent_abrahams@yahoo.com>
2015-06-09 01:03:36 +0800

    Added version constraints

--- package.js
+++ package.js
@@ -9,6 +9,7 @@ Package.describe({
 Npm.depends({'ldapjs' : '0.7.1', 'connect' : '2.19.3'});
 
 Package.on_use(function (api) {
+  api.versionsFrom('1.1.0.2');
   api.use(['routepolicy', 'webapp'], 'server');
   api.use(['accounts-base', 'underscore'], ['client', 'server']);
   api.use('accounts-password','server');
