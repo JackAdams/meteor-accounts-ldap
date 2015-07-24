578874aa706815c3fd825d73455a306c7df38813
Brent Abrahams <brent_abrahams@yahoo.com>
2015-07-24 17:03:36 +0200

    Version bump

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name : 'babrahams:accounts-ldap',
   summary: 'Meteor account login via LDAP',
-  version: '0.2.1',
+  version: '0.2.2',
   git : 'https://github.com/JackAdams/meteor-accounts-ldap',
   documentation: 'README.md'
 });
