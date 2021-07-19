3b7f425dd13130b5c04416eff8bc97e4fd09bbc6
Brent Abrahams <brent_abrahams@yahoo.com>
2021-07-19 13:47:12 +0800

    Update package.js

--- package.js
+++ package.js
@@ -1,24 +1,24 @@
 Package.describe({
   name : 'babrahams:accounts-ldap',
   summary: 'Meteor account login via LDAP',
-  version: '0.9.7',
+  version: '0.9.8',
   git : 'https://github.com/JackAdams/meteor-accounts-ldap',
   documentation: 'README.md'
 });
 
 Npm.depends({'ldapjs' : '1.0.2', 'connect' : '2.19.3'});
 
-Package.on_use(function (api) {
-  api.versionsFrom('1.8.2');
+Package.onUse(function (api) {
+  api.versionsFrom(['1.8.2', '2.3']);
   api.use(['routepolicy', 'webapp'], 'server');
   api.use(['accounts-base', 'underscore', 'less@3.0.1'], ['client', 'server']);
   api.use('accounts-password', 'server');
   api.imply('accounts-base', ['client', 'server']);
   api.use(['templating@1.3.2','blaze@2.3.4','spacebars@1.0.15','jquery@1.11.11', 'reactive-var'], 'client');
-  api.add_files([
+  api.addFiles([
     'ldap_client.html',
     'ldap_client.js',
     'ldap_client.less'], 'client');
-  api.add_files('ldap_server.js', 'server');
+  api.addFiles('ldap_server.js', 'server');
   api.export('LDAP');
 });
